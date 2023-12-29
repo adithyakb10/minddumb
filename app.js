32ef52d213f6d528030f0bf673d7bec49d219565
adithyakb10 <132006470+adithyakb10@users.noreply.github.com>
2023-12-29 08:37:06 +0530

    add page for submitting thoughts

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 import express from "express";
 import ejs from "ejs";
+import { Strategy } from "passport-google-oauth20";
+
+const GoogleStrategy = Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://www.example.com/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
 
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
 
 // Routes
-app.get("/", (req, res) => {
-  res.render("home.ejs");
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
 });
 
 app.listen(port, () => {
